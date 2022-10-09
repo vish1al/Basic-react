c4a8407db0141017350b9584f498a005a9ade717
vish1al <115404061+vish1al@users.noreply.github.com>
2022-10-09 17:24:57 +0530

    new project

---
+++ LEARN 1/index.js
@@ -0,0 +1,15 @@
+function NavBar()
+{
+return(
+<div ClassName="navbar">
+<p>hello world</p>
+<p>great working</p>
+</div>
+)
+}
+
+ReactDOM.render(
+<div>
+    
+<NavBar/>
+</div>,document.getElementById("root"));
